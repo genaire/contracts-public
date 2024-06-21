e326f75f4d701673235bdfcc39356af61e9075e9
genaire <genaire@engineer.com>
2024-06-21 10:00:14 +0100

    Update and rename IxRenzoDeposit.sol to IxGenaireDeposit.sol

--- contracts/Bridge/L2/IxGenaireDeposit.sol
+++ contracts/Bridge/L2/IxGenaireDeposit.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
 
-interface IxRenzoDeposit {
+interface IxGenaireDeposit {
     function deposit(
         uint256 _amountIn,
         uint256 _minOut,
