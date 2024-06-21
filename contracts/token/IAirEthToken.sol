ddf80f470ca103229a0caa8b40a3c815b0d9383d
genaire <genaire@engineer.com>
2024-06-21 09:26:35 +0100

    Update and rename IEzEthToken.sol to IAirEthToken.sol

--- contracts/token/IAirEthToken.sol
+++ contracts/token/IAirEthToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
-interface IEzEthToken is IERC20Upgradeable {
+interface IAirEthToken is IERC20Upgradeable {
     function mint(address to, uint256 amount) external;
 
     function burn(address from, uint256 amount) external;
